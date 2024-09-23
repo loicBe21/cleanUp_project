ac2a14f905501728d7525a35b8ed0611e1579f03
loicBe21 <loicRavelo05@gmail.com>
2024-09-23 16:14:09 +0200

    second update

---
+++ src/config/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  folders: [
+    {
+      path: "/chemin/a/surveiller",
+      keepRecentFiles: 5,
+      extensionsToDelete: [".zip", ".log"],
+    },
+    // Ajouter d'autres dossiers si nécessaire
+  ],
+};
